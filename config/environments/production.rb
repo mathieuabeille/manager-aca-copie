96b294ad072aed5e618410b168de09fdac4a4be0
Mathieu Abeille <mathieu.abeille@gmail.com>
2020-06-09 17:29:01 +0200

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ config.action_mailer.default_url_options = { host: host }
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => <gmail_username>,
-  :password             => <gmail_password>,
+  :user_name            => 'app.acaconsult@gmail.com',
+  :password             => '123/almavala',
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
